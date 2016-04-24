d7eaf6280a62a6ad6c2a1027d2550f6148a5183e
John Moreland <jmoreland@outlook.com>
2016-04-24 13:26:27 -0400

    Initial commit

---
+++ hill_cypher.py
@@ -0,0 +1,45 @@
+"""
+Code that encodes and deciphers hill ciphers
+@authors: John Moreland and Jeremy Garcia
+"""
+
+import math
+import numpy as np
+
+
+alphabet_dictionary = {' ':0, 'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 
+                       'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18,
+                        's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
+
+def is_invertible(key):
+    '''
+    Verifies that the given matrix is is_invertible
+    Output: Boolean
+    '''
+    pass
+
+def string_to_matrix(plaintext, dimension):
+    '''
+    Converts an input string into a matrix of numbers
+    '''
+    pass
+
+def invert_matrix(key):
+    '''
+    Inverts the input matrix
+    '''
+    pass
+
+def generate_key(dimension):
+    '''
+    Creates a square matrix with given dimensions
+    ''' 
+
+def create_cipher(plaintext, key, dimension=2):
+    '''
+    Generates ciphertext from the given plaintext and corresponding key
+    '''
+    pass
+    #map each letter to a number
+
+    #matrix moperation on the number
