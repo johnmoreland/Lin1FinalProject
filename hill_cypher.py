f70694ca3706d58b829ed9b5c663db1d15e4d1ec
John Moreland <jmoreland@outlook.com>
2016-04-24 14:19:21 -0400

    Implemented is_invertible and string_to_matrix functions

--- hill_cypher.py
+++ hill_cypher.py
@@ -6,21 +6,39 @@ Code that encodes and deciphers hill ciphers
 import math
 import numpy as np
 
-
-alphabet_dictionary = {' ':0, 'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 
-                       'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18,
-                        's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
+#chr and ord are our friends
 
 def is_invertible(key):
     '''
     Verifies that the given matrix is is_invertible
     Output: Boolean
     '''
-    pass
+    # Checks if determinant is nonzero
+    determinant = np.linalg.det(key)
+    return bool(determinant)
 
 def string_to_matrix(plaintext, dimension):
     '''
     Converts an input string into a matrix of numbers
+    '''
+    # Map each character to an ASCII value
+    ASCII_list = [ord(char) for char in plaintext]
+    
+    # Fill in empty elements
+    if len(ASCII_list) % dimension:
+        for i in range(len(ASCII_list) % dimension):
+            ASCII_list.append(32)
+
+    # Convert list into matrix with given dimension
+    ASCII_array = np.array(ASCII_list)
+    size = (len(ASCII_list) / dimension), dimension  #height, width
+    return np.reshape(ASCII_array, size).T
+
+
+
+def matrix_to_string(matrix, dimension):
+    '''
+    converts a matrix to a string
     '''
     pass
 
@@ -35,11 +53,23 @@ def generate_key(dimension):
     Creates a square matrix with given dimensions
     ''' 
 
-def create_cipher(plaintext, key, dimension=2):
+def encipher(plaintext, key, dimension=2):
     '''
     Generates ciphertext from the given plaintext and corresponding key
     '''
     pass
+    #Create a key if needed,
+
+    #check if key is invertible
+
     #map each letter to a number
 
     #matrix moperation on the number
+
+def decipher(ciphertext, key, dimension=2):
+    '''
+    Deciphers into plaintext
+    '''
+    pass
+
+print string_to_matrix("jeremy is lame!", 2)
