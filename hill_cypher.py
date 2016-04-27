cd707875ce9f09aca0b8f3d9e4648d8b1badf2d8
Jeremy Garcia <jeremiah.garcia@students.olin.edu>
2016-04-27 17:09:29 -0400

    I DID IT

--- hill_cypher.py
+++ hill_cypher.py
@@ -5,7 +5,7 @@ Code that encodes and deciphers hill ciphers
 
 import math
 import numpy as np
-import mod_inverse
+from mod_inverse import modinv
 
 #chr and ord are our friends
 
@@ -17,6 +17,12 @@ def is_invertible(matrix):
     # Checks if determinant is nonzero
     determinant = np.linalg.det(matrix)
     return bool(determinant)
+
+def is_modular(matrix):
+    det = np.linalg.det(matrix)
+    det_mod = int(round(det % 128))
+    test = modinv(det_mod,128)
+    return bool(test)
 
 def string_to_matrix(plaintext, dimension):
     '''
@@ -61,14 +67,12 @@ def invert_matrix(matrix):
     '''
     matrix = (np.linalg.inv(matrix))
     det = 1/np.linalg.det(matrix)
-    print det
-    det_mod = (det % 128)
-    print det_mod
+    det_mod = int(round(det % 128))
     matrix = np.multiply(matrix,det)
-
     matrix = np.mod(matrix,128)
-    #modinverse = mod_inverse.modinv(det_mod,128)
-    matrix = np.multiply(matrix,71)
+    #finds modular inverse
+    modinverse = modinv(det_mod,128)
+    matrix = np.multiply(matrix,modinverse)
     matrix = np.mod(matrix,128)
     return matrix
 
@@ -76,19 +80,20 @@ def generate_key(dimension):
     '''
     Creates a square matrix with given dimensions
     '''
-    # key = np.random.randint(256, size=(dimension,dimension))
-    # if is_invertible(key):
-    # 	key = np.mod(key,128)
-    # 	return key
-    # #if not invertible, generate a new key
-    # return generate_key(dimension)
+    key = np.random.randint(256, size=(dimension,dimension))
 
-    if dimension == 2:
-    	key = np.array([[1, 2],[2, 3]])
-    elif dimension ==3:
-    	#key = np.array([[7,2,1],[0,3,-1],[-3,4,-2]])
-    	key = np.array([[0,11,15],[7,0,1],[4,19,0]])
-    return key
+    if is_modular(key) and is_invertible(key):
+        key = np.mod(key,128)
+        return key
+    #if not invertible or if not modular, generate a new key
+    return generate_key(dimension)
+
+    # if dimension == 2:
+    # 	key = np.array([[1, 2],[2, 3]])
+    # elif dimension ==3:
+    # 	#key = np.array([[7,2,1],[0,3,-1],[-3,4,-2]])
+    # 	key = np.array([[0,11,15],[7,0,1],[4,19,0]])
+    # return key
 
 def encipher(plaintext, dimension=2):
     '''
@@ -128,8 +133,8 @@ def decipher(ciphertext, key, dimension=2):
     plaintext = matrix_to_string(new, dimension)
     return plaintext
 
-ciphertext, key = encipher('jeremyisawesome',3)
+ciphertext, key = encipher('jeremy is the greatest at hill ciphers',4)
 print 'ciphertext:',ciphertext
 print key
 print invert_matrix(key)
-print 'deciphered:',decipher(ciphertext,key,3)
+print 'deciphered:',decipher(ciphertext,key,4)
